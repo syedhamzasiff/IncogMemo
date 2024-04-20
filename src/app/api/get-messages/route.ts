d0080f9aa064db42e629361cb5a9b935deb9c8ff
syedhamzasiff <syedhamzasiff@gmail.com>
2024-04-21 00:54:35 +0500

    message API with aggregation pipeline

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,62 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+    await dbConnect();
+
+    const session = await getServerSession(authOptions)
+
+    const user: User = session?.user
+
+    if (!session || !user) {
+        return Response.json({
+            success: false,
+            message: "Not Authenticated"
+            },
+            {status: 401}
+        )
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+
+    try {
+        const user = await UserModel.aggregate([
+            { $match: {id: userId}},
+            {$unwind: '$messages'},
+            {$sort: {'messages.createdAt': -1}},
+            {$group: {_id: '$_id', messages: {$push: '$messages'}}}
+        ])
+
+        if (!user || user.length === 0) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+                },
+                {status: 500}
+            ) 
+        }
+        return Response.json({
+            success: true,
+            messages: user[0].messages
+            },
+            {status: 200}
+        ) 
+
+    } catch (error) {
+        console.error("An unexpected error occurred: ", error)
+        return Response.json({
+            success: false,
+            message: "An unexpected error occurred: ", error
+            },
+            {status: 500}
+        )
+    }
+
+    
+
+
+}
