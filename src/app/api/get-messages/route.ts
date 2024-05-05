2ed72c929caaef0183b302135e1f2a9ae9dd343c
syedhamzasiff <syedhamzasiff@gmail.com>
2024-05-06 00:35:12 +0500

    get-message | send-message bugs

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,62 +1,65 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
-import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/model/User";
-import { User } from "next-auth";
-import mongoose from "mongoose";
+import dbConnect from "@/lib/dbConnect"
+import UserModel from "@/model/User"
+import { User } from "next-auth"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]/options"
+import mongoose from "mongoose"
 
-export async function GET(request: Request) {
-    await dbConnect();
+export async function GET() {
+  await dbConnect()
 
-    const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions)
+  const _user: User = session?.user as User
+  if (!session || !_user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated"
+      },
+      {
+        status: 401
+      }
+    )
+  }
+  const userId = new mongoose.Types.ObjectId(_user._id)
+  try {
+    const userMessages = await UserModel.aggregate([
+      { $match: { _id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } }
+    ]).exec()
 
-    const user: User = session?.user
-
-    if (!session || !user) {
-        return Response.json({
-            success: false,
-            message: "Not Authenticated"
-            },
-            {status: 401}
-        )
-    }
-
-    const userId = new mongoose.Types.ObjectId(user._id);
-
-    try {
-        const user = await UserModel.aggregate([
-            { $match: {id: userId}},
-            {$unwind: '$messages'},
-            {$sort: {'messages.createdAt': -1}},
-            {$group: {_id: '$_id', messages: {$push: '$messages'}}}
-        ])
-
-        if (!user || user.length === 0) {
-            return Response.json({
-                success: false,
-                message: "User not found"
-                },
-                {status: 500}
-            ) 
+    if (!userMessages || userMessages.length === 0) {
+      return Response.json(
+        {
+          success: true,
+          message: "No Messages"
+        },
+        {
+          status: 200
         }
-        return Response.json({
-            success: true,
-            messages: user[0].messages
-            },
-            {status: 200}
-        ) 
-
-    } catch (error) {
-        console.error("An unexpected error occurred: ", error)
-        return Response.json({
-            success: false,
-            message: "An unexpected error occurred: ", error
-            },
-            {status: 500}
-        )
+      )
     }
-
-    
-
-
+    return Response.json(
+      {
+        success: true,
+        messages: userMessages[0].messages
+      },
+      {
+        status: 200
+      }
+    )
+  } catch (error) {
+    console.log("Internal server error: ", error)
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server error"
+      },
+      {
+        status: 500
+      }
+    )
+  }
 }
--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
 
         user.messages.push(newMessage as Message)
 
-        await user.save
+        await user.save()
 
         return Response.json({
             success: true,
